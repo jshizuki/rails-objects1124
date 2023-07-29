1e6e0c2652a094e87d148f335969e599e6f627c9
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-29 23:27:19 +0900

    Created show page for invoice

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,23 +3,34 @@ class InvoicesController < ApplicationController
     @invoices = Invoice.all
   end
 
-  # def create
-  # end
-
   # Creating a new invoice whenever there's a transaction
   def new
     @invoice = Invoice.new
   end
 
+  def create
+    @invoice = Invoice.new(invoice_params)
+    @invoice.save
+    redirect_to invoice_path(@invoice)
+  end
+
   # def edit
   # end
 
-  # def show
-  # end
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
 
   # def update
   # end
 
   # def destroy
   # end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:order_date, :billed_to)
+  end
+
 end
