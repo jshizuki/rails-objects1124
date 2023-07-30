57d591b3ea09509e7306bf8e432415690a1444cb
Joyce Lau <jshizuki@yahoo.com.hk>
2023-07-31 00:31:15 +0900

    Modified form

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,38 +5,13 @@ class InvoicesController < ApplicationController
 
   def new
     @invoice = Invoice.new
-    # This will create a new, unsaved product instance
-    # @invoice.products.build
   end
-
-  # def create
-  #   @invoice = Invoice.new(invoice_params)
-  #   # @product = Product.find_by_sku(invoice_params[:products_attributes].values.map { |product| product[:sku] })
-
-  #   if @invoice.save
-  #     sku_values = invoice_params[:products_attributes].values.map { |product| product[:sku] }
-  #     @products = Product.where(sku: sku_values)
-  #     @products.each do |product|
-  #       product.update(invoice_id: @invoice.id)
-  #     end
-  #     redirect_to invoice_path(@invoice)
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
 
   def create
     @invoice = Invoice.new(invoice_params)
 
     if @invoice.save
-      sku_values = invoice_params[:products_attributes].values.map { |product| product[:sku] }
-      # Retrieve product instances except the last one created from new
-      @products = Product.where(sku: sku_values)[1..]
-
-      @products.each do |product|
-        product.update(invoice: @invoice)
-      end
-
+      @invoice.products = Product.where(id: params[:invoice][:product_ids])
       redirect_to invoice_path(@invoice)
     else
       render :new, status: :unprocessable_entity
