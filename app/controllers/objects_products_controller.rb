7f9fe23d8f389413000bf547e4f7e152a8f6b08d
Joyce Lau <jshizuki@yahoo.com.hk>
2023-08-28 19:23:04 +0900

    Basic new and create method for products

--- app/controllers/objects_products_controller.rb
+++ app/controllers/objects_products_controller.rb
@@ -2,4 +2,34 @@ class ObjectsProductsController < ApplicationController
   def index
     @products = ObjectsProduct.all
   end
+
+  def new
+    @product = ObjectsProduct.new
+  end
+
+  def create
+    @product = ObjectsProduct.new(product_params)
+    @product.sku = calculate_sku
+    if @product.save
+      redirect_to objects_products_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:objects_product).permit(
+      :name,
+      :unit_price
+    )
+  end
+
+  def calculate_sku
+    previous_sku = ObjectsProduct.maximum(:sku)
+    numeric_part = previous_sku[3..].to_i
+    incremented_numeric_part = (numeric_part + 1).to_s.rjust(previous_sku.length - 3, '0')
+    "OBJ#{incremented_numeric_part}"
+  end
 end
