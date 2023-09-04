5ebd5739c7c8b22952ad8a23e333f7a73b961e8e
Joyce Lau <jshizuki@yahoo.com.hk>
2023-09-04 15:34:44 +0900

    Changed User to ObjectsUser

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_082150) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_060543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "objects_invoices", force: :cascade do |t|
     t.date "order_date"
@@ -37,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_082150) do
     t.index ["objects_invoice_id"], name: "index_objects_products_on_objects_invoice_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "objects_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -45,8 +73,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_082150) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_objects_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_objects_users_on_reset_password_token", unique: true
   end
 
   create_table "wlist_bookmarks", force: :cascade do |t|
@@ -86,6 +114,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_082150) do
     t.index ["reset_password_token"], name: "index_wlist_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "objects_products", "objects_invoices"
   add_foreign_key "wlist_bookmarks", "wlist_lists"
   add_foreign_key "wlist_bookmarks", "wlist_movies"
